23a045f98573e51a37ac4408680f113a31e4b274
haoxl <1098262516@qq.com>
2018-12-15 17:57:05 +0800

    init

---
+++ components/public/header/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <el-row :gutter="15" class="m-header">
+    <el-col :span="6">
+      <geo/>
+    </el-col>
+    <el-col></el-col>
+    <el-col></el-col>
+  </el-row>
+</template>
+<script>
+import Geo from './geo'
+export default {
+  components: {
+    Geo
+  }
+}
+</script>
+
