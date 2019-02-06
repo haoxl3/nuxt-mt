bd47b631250e1642604b9ddd7d1453e9ce411743
haoxl <1098262516@qq.com>
2019-02-06 22:27:09 +0800

    register

--- server/dbs/config.js
+++ server/dbs/config.js
@@ -16,19 +16,19 @@ export default {
             return '1098262516@qq.com'
         },
         get pass() {
-            return ''//授权码
-        }
-    },
-    get code() {
-        return ()=>{
-            //随机生成2到6位验证码
-            return Math.random().toString(16).slice(2,6).toUpperCase()
-        }
-    },
-    get expire() {
-        //设置验证码1分钟过期
-        return () => {
-            return new Date().getTime() + 60*60*1000
+            return 'smnmovmfftoubafi'//授权码
+        },
+        get code() {
+            return ()=>{
+                //随机生成2到6位验证码
+                return Math.random().toString(16).slice(2,6).toUpperCase()
+            }
+        },
+        get expire() {
+            //设置验证码1分钟过期
+            return () => {
+                return new Date().getTime() + 60*60*1000
+            }
         }
     }
 }
--- server/interface/users.js
+++ server/interface/users.js
@@ -1,5 +1,5 @@
 import Router from 'koa-router'
-import Redis from 'koa-redis'
+import Redis from 'koa-redis'//将用户与验证码关联
 import nodeMailer from 'nodemailer'
 import User from '../dbs/models/users'
 import Passport from './utils/passport'
@@ -13,7 +13,7 @@ let router = new Router({
 //获取redis的客户端
 let Store = new Redis().client
 
-//从redis中存的验证拿出来验证
+//从redis中存的验证码拿出来验证
 router.post('/signup', async(ctx)=>{
     const {username,password,email,code} = ctx.request.body;
     if(code){
@@ -79,3 +79,105 @@ router.post('/signup', async(ctx)=>{
         }
     }
 })
+
+router.post('/singin', async(ctx, next) => {
+    return Passport.authenticate('local', function(err,user,info,status){
+        if(err){
+            ctx.body = {
+                code: -1,
+                msg: err 
+            }
+        }else{
+            if(user) {
+                ctx.body = {
+                    code: 0,
+                    msg: '登录成功',
+                    user
+                }
+                return ctx.login(user)
+            }else {
+                ctx.body = {
+                    code: 1,
+                    msg: info
+                }
+            }
+        }
+    })(ctx, next)
+})
+
+router.post('/verify', async(ctx, next)=>{
+    let username = ctx.request.body.username
+    const saveExpire = await Store.hget(`nodemail:${username}`,'expire')
+    if(saveExpire && new Date().getTime() - saveExpire < 0) {
+        ctx.body = {
+            code: -1,
+            msg: '验证请求过于频繁，1分钟内1次'
+        }
+        return false 
+    }
+    //发送邮件
+    let transporter = nodeMailer.createTransport({
+        host: Email.smtp.host,
+        port: 587,
+        secure: false,
+        auth: {
+            user: Email.smtp.user,
+            pass: Email.smtp.pass 
+        }
+    })
+    //接收邮件
+    let ko = {
+        code: Email.smtp.code(),
+        expire: Email.smtp.expire(),
+        email: ctx.request.body.email,
+        user: ctx.request.body.username
+    }
+    //邮件显示的信息
+    let mailOptions = {
+        from: `"认证邮件"<${Email.smtp.user}>`,
+        to: ko.email,
+        subject: '《高仿美团网全栈实践》注册码',
+        html: `您在《高仿美团网全栈实践》课程中注册，您的邀请码是${ko.code}`
+    }
+    await transporter.sendMail(mailOptions, (error, info)=>{
+        if(error) {
+            return console.log('error')
+        }else {
+            Store.hmset(`nodemail:${ko.user}`,'code',ko.code,'expire',ko.expire,'email',ko.email)
+        }
+    })
+    ctx.body = {
+        code: 0,
+        msg: '验证码已发送，可能会有延时，有效期1分钟'
+    }
+})
+
+router.get('/exit', async(ctx, next) => {
+    await ctx.logout()
+    if(!ctx.isAuthenticated()) {
+        ctx.body = {
+            code: 0
+        }
+    }else {
+        ctx.body = {
+            code: -1
+        }
+    }
+})
+
+router.get('/getUser', async(ctx) => {
+    if(ctx.isAuthenticated()) {
+        const {username, email} = ctx.session.passport.user 
+        ctx.body = {
+            user: username,
+            email 
+        }
+    }else {
+        ctx.body = {
+            user: '',
+            email: ''
+        }
+    }
+})
+
+export default router
